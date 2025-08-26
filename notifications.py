61f346e0e24edc75a954a9c3e192224f2a0605ff
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2025-08-26 14:58:46 -0500

    updates

--- notifications.py
+++ notifications.py
@@ -10,18 +10,36 @@ quotes = [
     "Thaks God! IT's Monday!"
 ]
 
-if __name__ == "__main__":
-    notification = client.create_notification(
-        title=choice(quotes),
-        subtitle="Team Standup",
-        icon="/Users/jorrick/zoom.png",
-        sound="Frog",
-        action_button_str="Join zoom meeting",
-        action_callback=lambda: print("Pressed action button"),
-    )
+# 1 hour default
+delay_epoch = [5]
 
-    # TODO: remove before making a running daemon
-    while client.get_notification_manager().get_active_running_notifications() > 0:
-        time.sleep(1)
-        print(f"Active number of notifications: {client.get_notification_manager().get_active_running_notifications()}")
-    client.stop_listening_for_callbacks()
+def update_delay(temp_epoch: list, new_delay: int):
+    print(f'inside `update_delay`, temp_epoch: {temp_epoch}, delay_epoch: {delay_epoch}')
+    temp_epoch[0] = new_delay
+
+if __name__ == "__main__":
+    while True:
+        temp_epoch = delay_epoch.copy()
+        print(f"beginning of while loop, temp_epoch: {temp_epoch}")
+        notification = client.create_notification(
+            title=choice(quotes),
+            # subtitle="Team Standup",
+            # icon="/Users/jorrick/zoom.png",
+            sound="Frog",
+            action_button_str="Snooze for 4 Hours",
+            action_callback=update_delay(temp_epoch, 10),
+            # reply_button_str="Snooze for 4 Hours",
+            # reply_callback=update_delay(temp_epoch, 10),
+            # snooze_button_str="Dismiss Message",
+            # reply_button_str="Delay Messages"
+        )
+
+        print(f'right before time.sleep, temp_epoch: {temp_epoch}')
+        time.sleep(temp_epoch[0])
+        print(f'right after time.sleep, temp_epoch: {temp_epoch}')
+
+    # Debug loop
+    # while client.get_notification_manager().get_active_running_notifications() > 0:
+    #     time.sleep(1)
+    #     # print(f"Active number of notifications: {client.get_notification_manager().get_active_running_notifications()}")
+    # client.stop_listening_for_callbacks()
